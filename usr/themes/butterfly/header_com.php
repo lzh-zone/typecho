1a17affb017962f2141ed10fc6bba5a13ac0ebb7
sterling <102415551+lzh-zone@users.noreply.github.com>
2023-02-16 12:08:56 +0800

    Update header_com.php

--- usr/themes/butterfly/header_com.php
+++ usr/themes/butterfly/header_com.php
@@ -11,6 +11,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="renderer" content="webkit">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aplayer@1.10.1/dist/APlayer.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/aplayer@1.10.1/dist/APlayer.min.js"></script>
     <title><?php $this->archiveTitle(array(
             'category'  =>  _t('分类 %s 下的文章'),
             'search'    =>  _t('包含关键字 %s 的文章'),
